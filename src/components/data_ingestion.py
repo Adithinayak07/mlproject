dc443ede3ec63164c9c3b638c31abddacc4d9b1c
Adithinayak07 <adithinayak0710@gmail.com>
2026-01-18 23:00:03 +0530

    Hyperparameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -33,7 +33,7 @@ class DataIngestion:
             train_set,test_set = train_test_split(df,test_size=0.2,random_state =42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            train_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
 
@@ -53,3 +53,4 @@ if __name__ == "__main__":
 
     modeltrainer = ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
